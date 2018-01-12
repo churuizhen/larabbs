8f9dd2ade702c16c3f476b274492a87d1393a7f0
churuizhen <1298852829@qq.com>
2018-01-12 14:11:47 +0800

    UserPolicy

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,10 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
     //
     public function show(User $user)
     {
@@ -17,17 +21,21 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
     public function update(UserRequest $request,ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data=$request->all();
+
         if ($request->avatar){
             $result=$uploader->save($request->avatar,'avater',$user->id);
             if($result){
                 $data['avatar'] = $result['path'];
             }
         }
+
         $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
