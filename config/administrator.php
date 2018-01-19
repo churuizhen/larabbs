ee780fdc0c0053feca53502e871965447fe8c0d3
churuizhen <1298852829@qq.com>
2018-01-19 15:24:28 +0800

    admin

--- config/administrator.php
+++ config/administrator.php
@@ -80,7 +80,12 @@ return array(
             'users',
             'roles',
             'permissions'
-        ]
+        ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies',
+        ],
     ],
 
     /*
